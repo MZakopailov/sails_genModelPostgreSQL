c2a17890098595bb5ae0b8899dd8b44ebb995f91
Maxim Zakopajlov <maxim.zakopajlov@gmail.com>
2015-12-04 16:34:25 +0300

    create models from DB;

---
+++ app.js
@@ -0,0 +1,90 @@
+//
+//  app.js
+//  GetMeModelBro
+//
+//  Created by MAXIM ZAKOPAYLOV on 04.12.15.
+//  Copyright (c) 2015 Maxim Zakopaylov and Aleksey Rogov.
+//  All rights reserved.
+//
+
+var http = require('http');
+var pg = require('pg');
+var argv = require('minimist')(process.argv.slice(2));
+var helper = require('./helper');
+var fs = require('fs');
+
+var dirModel = './models';
+if (!fs.existsSync(dirModel)){
+    fs.mkdirSync(dirModel);
+}
+
+helper.checkArgInput(argv);
+var conString = "postgres://"+argv.USERNAME+":"+argv.PASSWORD+"@"+argv.HOST+"/"+argv.DBNAME;
+
+pg.connect(conString, function(err, client, done) {
+	if (err) {
+		console.log("Error pg client:" + err);
+		process.exit(1);
+	}
+	client.query("SELECT table_name FROM information_schema.tables WHERE table_schema='"+argv.SCHEMA+"'", function(err, result){
+		if (err){
+			console.log("Error pg client:" + err);
+			process.exit(1);
+		}
+		for (var i in result.rows){
+			compliteMode(client, result.rows[i].table_name, argv.SCHEMA);
+		}
+	});
+});
+
+//Get row name in table
+function compliteMode(pgClient, tablename, shema){
+	var resultJson = {
+		autoCreatedAt: false,
+		autoUpdatedAt: false,
+		tableName: ""+tablename+"",
+		connection: 'someMysqlServer',
+		migrate: 'safe',
+		attributes: {}
+	};
+	pgClient.query("SELECT * FROM information_schema.columns WHERE table_schema = '"+shema+"' AND table_name = '"+tablename+"'", function(err, result) {
+		var attributes = {};
+		var arrayResult = result.rows;
+		for (var i in arrayResult){
+			var row = result.rows[i];
+			attributes[row.column_name] = {};
+			attributes[row.column_name].type = helper.checkType(row.data_type).toUpperCase();
+		}
+		complitePrimaryKey(pgClient, tablename, attributes, function(attr){
+			compliteForeignKey(pgClient, tablename, attr, function(resultAttr){
+				resultJson.attributes = resultAttr;
+				fs.writeFile(dirModel +'/'+helper.capitalizeFirstLetter(tablename+'.js'),'module.exports = \n'+ JSON.stringify(resultJson,null,4) +';', function(err){
+					if (err) {
+						console.log("Error make file! " + err);
+						process.exit(1);
+					}
+					console.log(tablename + " Compited!");
+				});
+			});
+		});
+	});
+}
+
+function complitePrimaryKey(pgClient, tableName, attributes, callback){
+	pgClient.query("SELECT a.attname, format_type(a.atttypid, a.atttypmod) AS data_type FROM pg_index i JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey) WHERE i.indrelid = '"+tableName+"'::regclass AND i.indisprimary", function(err, result) {
+		for (var i in result.rows){
+			attributes[result.rows[i].attname].primaryKey = true
+		}
+		callback(attributes);
+	});
+}
+
+function compliteForeignKey(pgClient, tableName, attributes, callback){
+	pgClient.query("SELECT tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='"+tableName+"'", function(err, result) {
+		for (var i in result.rows){
+		 	attributes[result.rows[i].column_name].model = helper.capitalizeFirstLetter(result.rows[i].foreign_table_name)
+			attributes[result.rows[i].column_name].columnName = result.rows[i].column_name
+		}
+		callback(attributes);
+	});
+}
---
+++ helper.js
@@ -0,0 +1,53 @@
+//
+//  helper.js
+//  GetMeModelBro
+//
+//  Created by MAXIM ZAKOPAYLOV on 04.12.15.
+//  Copyright (c) 2015 Maxim Zakopaylov and Aleksey Rogov.
+//  All rights reserved.
+//
+
+
+module.exports = {
+  checkType: function(type){
+    switch (type) {
+      case 'real':
+        return 'FLOAT';
+        break;
+      case 'character varying':
+          return 'STRING';
+          break;
+      default:
+        return type;
+        break;
+    }
+  },
+
+  checkArgInput: function(argv){
+    if (argv.USERNAME == undefined) {
+    	console.log("ERROR: --USERNAME=<username>")
+    	process.exit(1);
+    }
+    if (argv.PASSWORD == undefined) {
+    	console.log("ERROR: --PASSWORD=<password>")
+    	process.exit(1);
+    }
+    if (argv.HOST == undefined) {
+    	console.log("ERROR: --HOST=<ip host>")
+    	process.exit(1);
+    }
+    if (argv.DBNAME == undefined){
+    	console.log("ERROR: --TABLEBANAME=<table name>")
+    	process.exit(1);
+    }
+    if (argv.SCHEMA == undefined){
+    	console.log("ERROR: --SCHEMA=<chema name>")
+    	process.exit(1);
+    }
+  },
+
+  capitalizeFirstLetter: function(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+}
